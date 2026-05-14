0c111bdce204ccffdbb21d0de3990039fad9b56d
anthonyochoa99 <aeochoa99@outlook.com>
2026-05-14 01:15:41 -0700

    Create a function to check if a key is an operator

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ const expression = {
 }
 
 const inputOutputContainer = document.querySelector(".input-output-container");
+
+function checkIfOperator(key) {
+    if (key === "/" ||
+        key === "*" ||
+        key === "+" ||
+        key === "-"
+    ) {
+        return true;
+    }
+}
