8d00b6a4038673ad5734cd502d4faad451bbd4d0
anthonyochoa99 <aeochoa99@outlook.com>
2026-05-14 01:06:46 -0700

    Create an object to store mathematical expression

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const inputKeys = document.querySelectorAll(".key");
 
+const expression = {
+    constant1: "",
+    operator: "",
+    constant2: "",
+    lastUpdated: "",
+    result: 0
+}
+
 const inputOutputContainer = document.querySelector(".input-output-container");
